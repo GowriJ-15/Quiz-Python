670eb631675dcdab257a6206366ba5bac221543e
GowriJ-15 <75430631+GowriJ-15@users.noreply.github.com>
2022-05-12 09:01:37 +0530

    Update pgm_file13.py

--- Python Quiz/pgm_file13.py
+++ Python Quiz/pgm_file13.py
@@ -1,25 +1,25 @@
 from datetime import datetime
 import time
-fin=open("answer.txt","a")
-fout=open("quizqns.txt","r")
-data=fout.readlines()
+fin=open("answer.txt","a") #file to store time,score
+fout=open("quizqns.txt","r") #file with questions 
+data=fout.readlines() #reading file line-wise
 #print(data)
 print('Hi welcome to Quiz')
 name=input("Enter your name")
 score=0
 for row in data:
-    Q=row.split('ans')
+    Q=row.split('ans') #splitting each line using 'ans'
     print("Question")
     print(Q[0])
     print('options :')
     print(Q[1])
     inp=input("Enter your answer-1,2,3,4 or pass")
     print(Q[2])
-    if inp.isalpha():
+    if inp.isalpha():   #if user passes the qn
         continue
-    elif int(inp)==int(Q[2]):
+    elif int(inp)==int(Q[2]): #if ans is correct 
         score=score+10
-    else:
+    else:                     #if ans is wrong 
         score=score-5
     op=input("Do you want to continue?y/n")
     if op=="n" or op=="N":
@@ -30,7 +30,7 @@ l=[name,"\t",str(score),"\t"]
 fin.writelines(l)
 fin.write("\t")
 date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
-fin.write(f"{date}")
+fin.write(f"{date}")  #timestamp
 fin.write("\n")
 fout.close()
 fin.close()
